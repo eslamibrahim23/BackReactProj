d36ec88a8bc3b8e0de1efcc5134bf144e3b942f3
Abdulrhaman-Mohamed <abdorrr2015@gmail.com>
2024-03-10 10:33:24 +0200

    remove Validition from screen

--- models/abdoUserSchema.js
+++ models/abdoUserSchema.js
@@ -27,36 +27,4 @@ const userabdoSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
-// Joi validation schema
-const validationSchema = Joi.object({
-  _id: Joi.required(),
-  email: Joi.string().email({
-    minDomainSegments: 2,
-    tlds: { allow: ["com", "net"] },
-  }),
-  password: JoiPassword.string()
-    .min(6)
-    .minOfNumeric(1)
-    .required(),
-  createdAt: Joi.date(),
-  updatedAt: Joi.date(),
-});
-
-// Mongoose pre-save hook for validation
-userabdoSchema.pre("save", function (next) {
-  const validation = validationSchema.validate(this.toObject());
-  if (validation.error) {
-    const err = validation.error.details[0].message;
-
-    return Promise.reject(
-      "Validation Error: " + validation.error.details[0].message
-    ); // Reject the Promise with the validation error
-    // Handle validation error (throw an error or handle it based on your application logic)
-    next(res.json({ err, status: "failed! " }));
-  } else {
-    // Validation successful, continue with the save operation
-    next();
-  }
-});
-
 module.exports = mongoose.model("abdoUser", userabdoSchema);
